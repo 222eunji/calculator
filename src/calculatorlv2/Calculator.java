a1a4f384e7ef3d33de09e45c055bf8e5ab16b26e
Eunji Lee <aggee212@gmail.com>
2025-03-05 18:55:42 +0900

    getter 적용 및 ã결과값 삭제 메서드 구현

--- src/calculatorlv2/Calculator.java
+++ src/calculatorlv2/Calculator.java
@@ -1,52 +1,52 @@
 package calculatorlv2;
+
+import java.util.List;
 import java.util.ArrayList;
 
 public class Calculator {
 
     // 1. 속성
-    private ArrayList<Integer> results = new ArrayList<>(); // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
-
+    private List<Integer> results = new ArrayList<>(); // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
 
     // 2. 생성자
-
 
     // 3. 기능
     // 덧셈 기능
     int sum(int a, int b) {
-    int result = a + b;
-    this.results.add(result);
+        int result = a + b;
+        this.results.add(result);
 
-    return result;
+        return result;
     }
 
 
     // 뺄셈 기능
     int sub(int a, int b) {
-    int result = a - b;
-    this.results.add(result);
+        int result = a - b;
+        this.results.add(result);
 
-    return result;
+        return result;
     }
 
 
     // 곱셈 기능
-    int mul(int a, int b){
-    int result = a * b;
-    this.results.add(result);
+    int mul(int a, int b) {
+        int result = a * b;
+        this.results.add(result);
 
-    return result;
+        return result;
     }
 
 
     // 나눗셈 기능
     int div(int a, int b) {
-    if (b == 0) {
-        throw new ArithmeticException("나눗셈 오류");
-    }
-    int result = a / b;
-    this.results.add(result);
+        if (b == 0) {
+            throw new ArithmeticException("나눗셈 오류");
+        }
+        int result = a / b;
+        this.results.add(result);
 
-    return result;
+        return result;
     }
 
 
@@ -54,24 +54,61 @@ public class Calculator {
     int calculator(int a, int b, char op) {
         int result = 0;
 
-      switch (op) {
-          case '+':
-          result = sum(a, b);
-          break;
+        switch (op) {
+            case '+':
+                result = sum(a, b);
+                break;
 
-          case '-':
-          result = sub(a, b);
-          break;
+            case '-':
+                result = sub(a, b);
+                break;
 
-          case '*':
-          result =  mul(a, b);
-          break;
+            case '*':
+                result = mul(a, b);
+                break;
 
-          case '/':
-          result =  div(a, b);
-          break;
-      }
+            case '/':
+                result = div(a, b);
+                break;
+        }
 
-      return result;
-      }
+        return result;
     }
+
+
+    // Getter
+    public List<Integer> getResults() {
+        return this.results;
+    }
+
+
+    // 연산 결과 데이터를 삭제하는 기능
+    public void removeResult(char del) {
+        switch (del) {
+            case 'A':
+                System.out.println("삭제를 하지 않습니다.");
+                break;
+
+            case 'B':
+                System.out.println("최근 연산 결과를 삭제합니다. ('" + results.get(results.size() - 1) + "' 삭제)");
+                results.remove(results.size() - 1);
+                break;
+
+            case 'C':
+                System.out.println("첫번째 데이터를 삭제합니다. ('" + results.get(0) + "' 삭제)");
+                results.remove(0);
+                break;
+
+            case 'D':
+                System.out.println("전체 데이터를 삭제합니다.");
+                results.clear();
+                break;
+
+            default:
+                System.out.println("※ 올바른 값을 입력하세요.\n");
+
+        }
+
+    }
+
+}
--- src/calculatorlv2/Main.java
+++ src/calculatorlv2/Main.java
@@ -1,4 +1,5 @@
 package calculatorlv2;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,76 +7,100 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
-        while(true) {
+        while (true) {
             // 연산을 수행할 값, 기호 받기
             int num1;
             int num2;
             char op;
 
-            while(true){ // 첫번째 값 입력 및 검증
-                System.out.print("첫 번째 값을 입력해주세요: ");
-                try {
-                    num1 = sc.nextInt();
-                    sc.nextLine();
-                } catch (Exception e){
-                    System.out.print("　※ 정수를 입력해주세요!\n");
-                    sc.nextLine();
-                    continue;
-                }
-                break;
-            }
+            while (true) { // 정수, 기호 입력 및 사칙연산 수행
 
-            while (true) { // 부호 입력 및 검증
-                System.out.print("+, -, *, / 중 1개를 입력해주세요: ");
-                try {
-                    op = sc.next().charAt(0);
-                    if (op != '+' && op != '-' && op != '*' && op != '/') {
-                        throw new Exception();
+                while (true) { // 첫번째 값 입력 및 검증
+                    System.out.print("1. 첫 번째 값을 입력해주세요: ");
+                    try {
+                        num1 = sc.nextInt();
+                        sc.nextLine();
+                    } catch (Exception e) {
+                        System.out.print("　※ 정수를 입력해주세요!\n");
+                        sc.nextLine();
+                        continue;
                     }
-
-                } catch (Exception e){
-                    System.out.print("　※ 사칙연산 부호를 입력해주세요!\n");
-                    sc.nextLine();
-                    continue;
+                    break;
                 }
+
+                while (true) { // 부호 입력 및 검증
+                    System.out.print("2. +, -, *, / 중 1개를 입력해주세요: ");
+                    try {
+                        op = sc.next().charAt(0);
+                        if (op != '+' && op != '-' && op != '*' && op != '/') {
+                            throw new Exception();
+                        }
+
+                    } catch (Exception e) {
+                        System.out.print("　※ 사칙연산 부호를 입력해주세요!\n");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                }
+
+                while (true) { // 두번째 값 입력 및 검증
+                    System.out.print("3. 두 번째 값을 입력해주세요: ");
+                    try {
+                        num2 = sc.nextInt();
+                        sc.nextLine();
+                    } catch (Exception e) {
+                        System.out.print("　※ 정수를 입력해주세요!\n");
+                        sc.nextLine();
+                        continue;
+                    }
+                    break;
+                }
+
+
+            int result = 0; // 연산 결과 값 넣을 변수 선언
+
+            try {
+                result = calculator.calculator(num1, num2, op);
+                System.out.println("▷ 결과: " + num1 + op + num2 + "=" + result + "\n");
                 break;
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("　※ 나눗셈 연산에서 분모(두번째 값)에 0이 입력될 수 없습니다.\n 　　값을 다시 입력해주세요.\n");
+            }
+            } // 사칙 연산 완료
+
+
+            // 사친 연산 성공 이후
+            // 데이터 삭제 여부 확인
+            char del;
+            while (true) {
+                System.out.println("저장된 결과값을 삭제하겠습니까?");
+
+                // 연산 결과 기록 확인
+                System.out.println("계산 이력: " + calculator.getResults());
+
+                System.out.println("A.삭제하지 않음 / B.최근 연산 결과 삭제 / C.첫 번째 데이터 삭제 / D.전체 데이터 삭제");
+                del = sc.next().charAt(0);
+                sc.nextLine();
+                calculator.removeResult(del);
+
+                if (del == 'A' || del == 'B' || del == 'C' || del == 'D') {
+                    break;
+                }
             }
 
-            while(true){ // 두번째 값 입력 및 검증
-                System.out.print("두 번째 값을 입력해주세요: ");
-                try {
-                    num2 = sc.nextInt();
-                    sc.nextLine();
-                } catch (Exception e){
-                    System.out.print("　※ 정수를 입력해주세요!\n");
-                    sc.nextLine();
-                    continue;
-                }
+            // 계산 진행 여부 확인
+            System.out.print("\n더 계산하시겠습니까? (exit 입력 시 종료)\n입력: ");
+            String answer = sc.nextLine();
+            if (answer.equals("exit")) {
+                System.out.print("계산기를 종료합니다.");
                 break;
+            } else {
+                System.out.print("===================================");
             }
+            System.out.println(" ");
 
-        int result = 0; // 연산 결과 값 넣을 변수 선언
-
-         try {
-             result = calculator.calculator(num1, num2, op);
-             System.out.println("결과: " + num1 + op + num2 + "=" + result);
-
-             System.out.print("\n더 계산하시겠습니까? (exit 입력 시 종료)\n입력: ");
-             String answer = sc.nextLine();
-             if (answer.equals("exit")) {
-                 System.out.print("계산기를 종료합니다.");
-                 break;
-             } else {
-                 System.out.print("===================================");
-             }
-             System.out.println(" ");
-
-         }
-         catch (Exception e) {
-             System.out.println(e);
-             System.out.println("　※ 나눗셈 연산에서 분모(두번째 값)에 0이 입력될 수 없습니다.\n 　　값을 다시 입력해주세요.\n");
-         }
-
-        }
+        } // 계산기 전체 반복
     }
 }
